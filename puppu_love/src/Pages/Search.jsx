08350611bb38b2dc51683972a4bacd735f037fc7
PreetS12301~ <preetbhardwaj517@gmail.com>
2022-09-07 07:07:50 +0530

    layout-search-page-done

--- puppu_love/src/Pages/Search.jsx
+++ puppu_love/src/Pages/Search.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { FaSearch } from "react-icons/fa";
+import styled from 'styled-components'
 
 export const Search = () => {
   return (
-    <div>Search</div>
+    <MainDivWrapper>
+    <InnerDivWrapper>
+        <div>
+            <input type="text" id='breed' placeholder='Enter Breed Name' />
+        </div>
+        <div>
+        <button>
+            <FaSearch />
+        </button>
+    </div>
+    </InnerDivWrapper>
+    
+    </MainDivWrapper>
   )
 }
+
+const MainDivWrapper= styled.div`
+   width:50%;
+   height:fit-content;
+   padding:2%;
+   border:2px solid purple;
+   margin:20px auto;
+   display:flex;
+   align-items:center;
+   justify-content:center;
+
+`;
+
+const InnerDivWrapper= styled.div`
+   width:50%;
+   height:fit-content;
+   display:flex;
+   align-items:center;
+   justify-content:center;
+   gap:20px;
+
+`;
+
