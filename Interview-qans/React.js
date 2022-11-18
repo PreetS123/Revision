c7ebc193f4e8c35d45ae33687ec08c457c162912
PreetS123 <preetbhardwaj517@gmail.com>
2022-11-18 17:41:16 +0530

    react-q-a

--- Interview-qans/React.js
+++ Interview-qans/React.js
@@ -1,4 +1,4 @@
-  /*
+/*
        What is React.js?
        React is an efficient, flexible, open-source front-end javaScript
        library created by FaceBook in 2011. It is a tool for building 
@@ -9,7 +9,7 @@
        React.js is a client-side javaScript library which helps to build user-interface in terms of components.
   */
 
-   /*
+/*
         What are important features of React.js?
          Important features of react.js are:
           1).It supports server-side rendering.
@@ -19,7 +19,7 @@
           5). React uses JSX, a combination of basic HTML and JavaScript. 
    */
 
-  /*
+/*
        What are advantages of React.js ?
         1. Easy to learn and use.
         2. Unidirectional data flow.
@@ -27,7 +27,7 @@
         4. Use of Reusable components.
    */
 
-  /*
+/*
       What are limitations of React.js ?
         1). The first thing is that React is not a fully-fledged framework.
             It is just a JavaScript library.
@@ -35,32 +35,29 @@
         3). The coding might become complex using JSX.
   */
 
-
-  /*
+/*
        Hooks--> Hooks are a way to manage state in react components.
        They allow us to use all React features.
        Using Hooks, we can extract the stateful logic from a component so it can be tested independently and reused. 
        This makes it possible to share Hooks among many components.
     */
-  
-   
-   /*
+
+/*
      State Variable---> State variable is a variable whose value can be updated.
     */
 
-  /*
+/*
       What is useState in React.
        UseState is a react hooks and it is used to create a state variable with an initial value.
        This variable are monitered by VDOM and whenever the value of the variable changes VDOM will
        re-render the component.
       
    */
-  /*
+/*
     What is component in Reat.js?
   */
 
-  
-  /*
+/*
 
     UseRef---> useRef is a hook that store the value and 
                not loose the value when component is updated or re-render.
@@ -69,21 +66,18 @@
             
      */
 
-  /*
+/*
      When would you use useRef?
      We can use useRef, when we want to change our state but the value of state shouldn't get triggred on DOM.
     //  If we don't want to update DOM element but want to get a value we use useRef.
     */
 
-
-
-  /*
+/*
     How to access DOM element in React?
      By using useRef.
    */
-   
-  
-   /*
+
+/*
      Explain a difference between useState and useRef ?
       useState is a hook which is used to update the state in functional component.
 
@@ -163,7 +157,7 @@ setInterval---> SetInterval is a callback function which repeats a given functio
      It is being used for better code reusability and readability.
  
   */
-   
+
 /*
   Can we initialize a state with a function?
     Yes,
@@ -173,7 +167,7 @@ setInterval---> SetInterval is a callback function which repeats a given functio
    Do two component using same hook share State?
      No,
   */
-  
+
 /*
    When would you use useContext hook?
      When we need to write same logic at multiple place then we use useContext hooks for reducing multiple line
@@ -185,7 +179,6 @@ setInterval---> SetInterval is a callback function which repeats a given functio
     If a custom hook returns javaScript XML file then it will become similar to any other functional react hooks.
     Custom hook will return data.
    */
-
 
 /*
      What is memoization in react?
@@ -217,12 +210,11 @@ setInterval---> SetInterval is a callback function which repeats a given functio
       then memoize the value returned from the function passed into it.
     */
 
-
 /*
      Explain the difference between useMemo vs useEffect + useState usage in that code?
     */
 
-  /*
+/*
     What is Context Api in react.js?
     Context Api is a way to pass data from parent to child component(s),
     and consume data in whatever component we need in our application without using props,
@@ -230,7 +222,7 @@ setInterval---> SetInterval is a callback function which repeats a given functio
     
     */
 
-   /*
+/*
    Why React Context? We have Redux!!
    Using Redux intraction with states from parent to child components is quite difficult and
    it also gives us a complex code.
@@ -238,9 +230,7 @@ setInterval---> SetInterval is a callback function which repeats a given functio
    
    */
 
-
-
-  /*
+/*
   What is useReducer?
    useReducer hook is generally used when we have complex state-building logic
    or when the next state value depends upon its previous value
@@ -255,9 +245,7 @@ setInterval---> SetInterval is a callback function which repeats a given functio
    const [state, dispatch] = useReducer(reducer, initialArgs, init);
     */
 
-
-
-    /*
+/*
     What is useNavigate?
     The useNavigate() hook is introduced in the React Router v6 to replace the useHistory() hook.
      In it the React Router’s navigation API provides useNavigate() hook 
@@ -269,6 +257,52 @@ setInterval---> SetInterval is a callback function which repeats a given functio
      It helps to go to the specific URL, forward or backward pages. 
      */
 
-  /*
+/*
      What is class component?
    */
+
+/*
+       What is props?
+       React allows us to pass information to a Component 
+       (from a parent component to a child component) using something called props.
+        Props is basically an object which is available for all the React components.
+        Props are read only and cannot be modified by the component to which it belongs. 
+     */
+
+/*
+        What is prop drilling?
+         Prop drilling is basically a situation when the same data is being sent
+          at almost every level due to requirements in the final level. 
+        */
+
+/*
+          What is life-cycle-of-react.js?
+          Initialization: This is the stage where the component is constructed with the given Props and default state.
+           This is done in the constructor of a Component Class.
+           Mounting: Mounting is the stage of rendering the JSX returned by the render method itself.
+           Updating: Updating is the stage when the state of a component is updated and the application is repainted.
+          Unmounting: As the name suggests Unmounting is the final step of the component lifecycle where the component is removed from the page.
+          */
+
+/*
+          What is Components?
+          A Component is one of the core building blocks of React. 
+          In other words, we can say that every application you will
+           develop in React will be made up of pieces called components.
+           Components make the task of building UIs much easier. 
+          */
+
+/*
+           What is constructor function.
+           The constructor() method is a special method for creating and initializing objects created within a class.
+           The constructor() method is called automatically when a class is initiated,
+           and it has to have the exact name "constructor", in fact, if you do not have a constructor method,
+           JavaScript will add an invisible and empty constructor method.
+           
+             eg:- class Car {
+              constructor(brand) {  // Constructor
+                this.carname = brand;
+                }
+              }
+              mycar = new Car("Ford");
+           */
