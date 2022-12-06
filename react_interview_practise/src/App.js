dfe4e543132eb1eb23ff82a46f170682b3ee3c16
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-06 12:18:01 +0530

    timer-implemented

--- react_interview_practise/src/App.js
+++ react_interview_practise/src/App.js
@@ -1,39 +1,37 @@
-
-import './App.css';
-import { SetIntervalek1 } from './ExamplePractise/SetIntervalek1';
-import { UseRefEx } from './ExamplePractise/UseRefEx';
-import { Setintervalex } from './ExamplePractise/Setintervalex';
-import { Useeffectex } from './ExamplePractise/Useeffectex';
-import { Route, Routes } from 'react-router-dom';
-import { Todo } from './ExamplePractise/Todo';
+import "./App.css";
+import { SetIntervalek1 } from "./ExamplePractise/SetIntervalek1";
+import { UseRefEx } from "./ExamplePractise/UseRefEx";
+import { Setintervalex } from "./ExamplePractise/Setintervalex";
+import { Useeffectex } from "./ExamplePractise/Useeffectex";
+import { Route, Routes } from "react-router-dom";
+import { Todo } from "./ExamplePractise/Todo";
+import { Timer } from "./ExamplePractise/Timer";
 
 function App() {
- 
   return (
     <div className="App">
-    <Routes>
-      <Route path='/' element={<SetIntervalek1/>} />
-      <Route path='/useref' element={<UseRefEx/>} />
-      <Route path='/useeffect' element={<Useeffectex/>} />
-      <Route path='/setinterval' element={<Setintervalex/>} />
-      <Route path='/todo' element={<Todo/>} />
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-      {/* <Route path='/' element={} /> */}
-
-    </Routes>
+      <Routes>
+        <Route path="/" element={<SetIntervalek1 />} />
+        <Route path="/useref" element={<UseRefEx />} />
+        <Route path="/useeffect" element={<Useeffectex />} />
+        <Route path="/setinterval" element={<Setintervalex />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/timer" element={<Timer />} />
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+      </Routes>
     </div>
   );
 }
