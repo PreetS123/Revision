4424beee85a95684c15c7c7e4b2f4f160eac9064
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-15 21:01:30 +0530

    interview done

---
+++ Interview-react-repo/javaScript-file/q1.js
@@ -0,0 +1,14 @@
+let arr=['apple','mango','grapes','watermelon','muskmelon','pear'];
+
+let fruit='apple';
+
+// Ankit
+// tikna
+
+function reverseArray(arr){
+//    arr.sort();
+    arr.reverse();
+   return arr;
+}
+
+console.log(reverseArray(arr));
